322509600f18c6293ee2aff46cd5ea72261cb4e1
bma51 <bma51@txstate.edu>
2018-11-07 21:39:10 -0600

    cleaned up model imports, added a post and del for riders

--- app/routes.py
+++ app/routes.py
@@ -1,39 +1,73 @@
 from app import app, db
 from flask_restful import Resource, Api, reqparse
-from app.models import AdminModel
+from app.models import AdminModel, RiderModel, DriverModel
 from flask import jsonify, abort
 from sqlalchemy.exc import DatabaseError
-from app.serializers import admin_schema_many
+from app.serializers import admin_schema_many, rider_schema_many, driver_schema_many
 
 api = Api(app)
 
-'''
 class Driver(Resource):
     def __init__(self):
         parser = reqparse.RequestParser()
 
         parser.add_argument('id', type=int)
+        parser.add_argument('name', type=str)
 
         self.args = parser.parse_args()
 
         super().__init__()
 
     def get(self):
-        return {'hello': 'world'}
+        drivers = DriverModel.query.all()
+        print(drivers)
+        return jsonify(drivers=driver_schema_many.dump(drivers).data)
 
 class Rider(Resource):
     def __init__(self):
         parser = reqparse.RequestParser()
 
         parser.add_argument('id', type=int)
+        parser.add_argument('name', type=str)
 
         self.args = parser.parse_args()
 
         super().__init__()
 
+    def post(self):
+        try:
+            new_rider = RiderModel(**self.args)
+            db.session.add(new_rider)
+            db.session.commit()
+
+        except DatabaseError:
+            return abort(500, 'Rider was not added to the database!')
+
+        return jsonify(message='Rider successfully created!')
+
+    def delete(self):
+        rider = RiderModel.query.filter_by(id=Rider.args['id']).first()
+
+        if rider:
+            try:
+                db.session.delete(rider)
+                db.session.commit()
+            except DatabaseError:
+                return abort(502, 'The item was not deleted')
+
+            return jsonify(message="The rider was successfully deleted")
+
+        else:
+            return abort(503, 'The rider did not exist')
+
+
     def get(self):
-        return {'hello': 'world'}
-'''
+        riders = RiderModel.query.all()
+        print(riders)
+        return jsonify(riders=rider_schema_many.dump(riders).data)
+
+
+
 class Admin(Resource):
     def __init__(self):
         parser = reqparse.RequestParser()
@@ -94,6 +128,7 @@ class Admin(Resource):
         else:
             return abort(503, 'The admin did not exist')
 
-#api.add_resource(Driver, '/driver')
-#api.add_resource(Rider, '/rider')
+        
+api.add_resource(Driver, '/admin/driver')
+api.add_resource(Rider, '/admin/rider')
 api.add_resource(Admin, '/admin')
